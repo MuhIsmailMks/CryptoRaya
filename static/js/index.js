b6f357c7913900d68dda9ac3b5db37146cb01a20
muhammad ismail <muhismailmks@gmail.com>
2024-08-17 13:37:44 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -21,12 +21,18 @@ close.addEventListener('click',() => {
 const dropdown_menu = document.querySelector('.dropdown-menu');
  
 function toggleDropdown(event) {
-    event.stopPropagation();  body
+    event.stopPropagation(); 
     dropdown_menu.style.display = dropdown_menu.style.display === 'block' ? 'none' : 'block';
 }
  
 white_btn.addEventListener('click', toggleDropdown);
  
+// document.body.addEventListener('click', (event) => {
+//     if (!dropdown_menu.contains(event.target) && !white_btn.contains(event.target)) {
+//         dropdown_menu.style.display = 'none';
+//     }
+// });
+
 document.body.addEventListener('click', (event) => {
     if (!dropdown_menu.contains(event.target) && !white_btn.contains(event.target)) {
         dropdown_menu.style.display = 'none';
