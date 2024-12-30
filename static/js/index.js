5c093818f4563981303dc594579d90864cbf313c
muhammad ismail <muhismailmks@gmail.com>
2024-12-30 10:44:49 +0800

    update

---
+++ static/js/index.js
@@ -0,0 +1,47 @@
+//  scroll animation Effect
+window.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        once: true,
+        offset: 120,
+    });
+})
+
+
+// menuBtn
+const menuBtn = document.querySelector('.menuBtn'); 
+const menuContainer = document.querySelector('.menuContainer');
+const menuIcon = document.querySelector('#menuIcon');
+
+menuBtn.addEventListener('click', () => {
+    menuContainer.classList.toggle('active')
+
+    if (menuContainer.classList.contains('active')) {
+        menuIcon.src = './static/images/menuActive.svg';
+    } else {
+        menuIcon.src = './static/images/menuBtn.svg';
+    }
+
+})
+
+
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();  
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const offset = 100;
+            const targetPosition = targetElement.offsetTop - offset;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+
+        // menu
+        menuContainer.classList.remove('active')
+        menuIcon.src = './static/images/menuBtn.svg';
+    });
+});
